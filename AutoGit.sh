298f2ac9da5c9614919555cde02650b72ab9db7c
Nryder1102 <mprecanico@gmail.com>
2018-11-15 09:34:47 -0500

    Added AutoGit.sh | Bash Script to automatically push everything to Github

---
+++ AutoGit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Morgan Precanico
+#11/14/18
+#Github Automation
+
+cd /home/nyght/Desktop/GithubFolder/
+git add .
+read -p "Description for the changes?
+> " description
+git commit -m "$description"
+echo $description
+git push origin master
