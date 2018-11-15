9eb2c51c866bb6b637133a3a4e7e03e37c75c3d4
Nryder1102 <mprecanico@gmail.com>
2018-11-15 09:29:34 -0500

    Added name.py | Removed Herobrine

---
+++ name.py
@@ -0,0 +1,11 @@
+#Greeting Script
+#Morgan Precanico
+#11/15/18
+
+
+def name():
+	first_name = input("Enter your First Name: ")
+	last_name = input("Enter your Last Name: ")
+	print(" ")
+	print("Hello,",first_name, last_name,)
+	print("How are you?")
