632b6e15b433990755d38c3b517cb5809110a665
Nryder1102 <mprecanico@gmail.com>
2018-11-15 12:24:53 -0500

    Major Changes

--- AutoGit.sh
+++ AutoGit.sh
@@ -3,10 +3,33 @@
 #11/14/18
 #Github Automation
 
-cd /home/nyght/Desktop/GithubFolder/
+read -p "Have you completed the setup before? [Y/N]
+> " n
+case $n in
+
+	n)
+read -p "Please enter your PC's username so the script can make a library folder
+> " user
+read -p "Please enter the absolute file path to your github folder 
+> " path
+read -p "$path, is this correct? [Y/N]
+> " y
+case $y in
+	y)
+		mkdir /home/$user/Desktop/lib
+		echo "path="$path"" > /home/$user/Desktop/lib/AutoGitFilePath.txt
+		;;
+	n)
+		echo "Exiting Program..."
+		;;
+esac
+;;
+
+	y)
+source AutoGitFilePath.txt
+cd $path
 git add .
 read -p "Description for the changes?
 > " description
 git commit -m "$description"
-echo $description
 git push origin master
