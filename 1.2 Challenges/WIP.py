21a9079bd2e75d1580800e63a9a217e571d427d9
Nryder1102 <mprecanico@gmail.com>
2018-11-19 13:48:28 -0500

    Pushing code to work on at home

---
+++ 1.2 Challenges/WIP.py
@@ -0,0 +1,9 @@
+import random
+def madlibs():
+    for x in range(1):
+        rnoun = random.randint(1,6)
+        print(rnoun)
+         #weight = float(input("How many pounds does your suitcase weigh? "))
+ #  if weight > 50:
+  #      print("There is a $25 charge for luggage that heavy.")
+ #   print("Thank you for your business.")
