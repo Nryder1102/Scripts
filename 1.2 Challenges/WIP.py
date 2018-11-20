a088f91e3281cef8a6941e0ef57fc3d00c16e363
Nryder1102 <mprecanico@gmail.com>
2018-11-20 12:00:59 -0500

    WIP

--- 1.2 Challenges/WIP.py
+++ 1.2 Challenges/WIP.py
@@ -1,9 +1,21 @@
 import random
 def madlibs():
-    for x in range(1):
-        rnoun = random.randint(1,6)
+    for w in range(1):
+        rnoun = random.randint(1,4)
         print(rnoun)
-         #weight = float(input("How many pounds does your suitcase weigh? "))
- #  if weight > 50:
+
+    for x in range(1):
+        rverb = random.randint(1,4)
+        print(rverb)
+
+    for y in range(1):
+        radj = random.randint(1,4)
+        print(radj)
+
+    for z in range(1):
+        rplace = random.randint(1,4)
+        print(rplace)
+
+    #  if weight > 50:
   #      print("There is a $25 charge for luggage that heavy.")
  #   print("Thank you for your business.")
