31907410b10e003e1cb41b499a1cd0b77bf8ff2e
Nryder1102 <45008433+Nryder1102@users.noreply.github.com>
2018-12-10 13:53:49 -0500

    Add files via upload

---
+++ 1.2 Challenges/rover.py
@@ -0,0 +1,7 @@
+def rover():
+    print("How long do you think it'll take for a photo sent from")
+    guess = input("the Mars Curiosity Rover to reach earth? ")
+    answer = 34000000 / 186000
+    print("This is how long it takes for it to happen, was your guess close?")
+    print("Answer:",answer)
+    print("Guess:",guess)
