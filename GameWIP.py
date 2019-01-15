15f12e1504005c4daf79358c039cc304b5e73089
Nryder1102 <nryder1102@gmail.com>
2019-01-15 13:47:16 -0500

    Updated Game | Added Herobrine | Removed Herobrine

--- GameWIP.py
+++ GameWIP.py
@@ -24,6 +24,10 @@ def main():
     shapedown = Polygon(Point(50,0), Point(-50,0), Point(0, -75))
     shapedown.setOutline("light blue")
     shapedown.setFill("sky blue")
+    shapecenter = Circle(Point(0,0), 13)
+    shapecenter.setFill("black")
+    shapecenter.setOutline("silver")
+    shapecenter.setWidth(3)
     pts = "0"
     arm = "0"
     t = Text(Point(0,75), "Press 'Enter' to start!")
@@ -65,13 +69,13 @@ def main():
     e2movestate = 0
     enemy3 = Rectangle(Point(-20,500), Point(20,460))
     enemy3.setFill("black")
-    enemy3.setWidth(0.5)
+    enemy3.setWidth(0)
     enemy3state = 15
     e3drawstate = 0
     e3movestate = 0
     enemy4 = Rectangle(Point(-15,-500), Point(15,-470))
     enemy4.setFill("black")
-    enemy4.setWidth(0.5)
+    enemy4.setWidth(0)
     enemy4state = 15
     e4drawstate = 0
     e4movestate = 0
@@ -87,15 +91,16 @@ def main():
     buffer4 = 0
     buffer5 = 0
     armorstate = 0
-    key = win.getKey()
+    key2 = win.getKey()
 
 
     #Block to start game
-    if key == "Return":
+    if key2 != "Escape":
         t.undraw()
         ttwo.undraw()
         tthree.undraw()
-
+        key2 = 0
+        key = 0
     #Block to end game if Escape is pressed
         while key != "Escape":
             key = win.getKey()
@@ -106,17 +111,23 @@ def main():
                 shapeleft.undraw()
                 shaperight.undraw()
                 shapedown.undraw()
+                shapecenter.undraw()
                 if armorstate == 0:
+                    shapecenter.setOutline("black")
+                    shapecenter.setFill("sky blue")
                     shapeup.setOutline("sky blue")
                     shapeup.setFill("light blue")
-                    shapeup.setWidth(1)
+                    shapeup.setWidth(0)
                 elif armorstate != 0:
+                    shapecenter.setFill("lime green")
+                    shapecenter.setOutline("silver")
                     shapeup.setFill("purple")
                     shapeup.setOutline("silver")
                     shapeup.setWidth(5)
                 shapeup.draw(win)
                 shaperot = shapeup
                 shapestate = "shapeup.draw(win)"
+                shapecenter.draw(win)
                 e1movestate = 1
                 e2movestate = 1
                 e3movestate = 1
@@ -129,17 +140,23 @@ def main():
                 shapeleft.undraw()
                 shapedown.undraw()
                 shapeup.undraw()
+                shapecenter.undraw()
                 if armorstate == 0:
+                    shapecenter.setOutline("black")
+                    shapecenter.setFill("red")
                     shaperight.setOutline("red")
                     shaperight.setFill("dark red")
-                    shaperight.setWidth(1)
+                    shaperight.setWidth(0)
                 elif armorstate != 0:
+                    shapecenter.setFill("lime green")
+                    shapecenter.setOutline("silver")
                     shaperight.setFill("purple")
                     shaperight.setOutline("silver")
                     shaperight.setWidth(5)
                 shaperight.draw(win)
                 shaperot = shaperight
                 shapestate = "shaperight.draw(win)"
+                shapecenter.draw(win)
                 e1movestate = 1
                 e2movestate = 1
                 e3movestate = 1
@@ -152,17 +169,23 @@ def main():
                 shapeleft.undraw()
                 shaperight.undraw()
                 shapeup.undraw()
+                shapecenter.undraw()
                 if armorstate == 0:
+                    shapecenter.setOutline("black")
+                    shapecenter.setFill("light blue")
                     shapedown.setOutline("light blue")
                     shapedown.setFill("sky blue")
-                    shapedown.setWidth(1)
+                    shapedown.setWidth(0)
                 elif armorstate != 0:
+                    shapecenter.setFill("lime green")
+                    shapecenter.setOutline("silver")
                     shapedown.setFill("purple")
                     shapedown.setOutline("silver")
                     shapedown.setWidth(5)
                 shapedown.draw(win)
                 shaperot = shapedown
                 shapestate = "shapedown.draw(win)"
+                shapecenter.draw(win)
                 e1movestate = 1
                 e2movestate = 1
                 e3movestate = 1
@@ -175,17 +198,23 @@ def main():
                 shaperight.undraw()
                 shapedown.undraw()
                 shapeup.undraw()
-                if armorstate == 0: 
+                shapecenter.undraw()
+                if armorstate == 0:
+                    shapecenter.setOutline("black")
+                    shapecenter.setFill("dark red")
                     shapeleft.setOutline("dark red")
                     shapeleft.setFill("red")
-                    shapeleft.setWidth(1)
+                    shapeleft.setWidth(0)
                 elif armorstate != 0:
+                    shapecenter.setFill("lime green")
+                    shapecenter.setOutline("silver")
                     shapeleft.setFill("purple")
                     shapeleft.setOutline("silver")
                     shapeleft.setWidth(5)
                 shapeleft.draw(win)
                 shaperot = shapeleft
                 shapestate = "shapeleft.draw(win)"
+                shapecenter.draw(win)
                 e1movestate = 1
                 e2movestate = 1
                 e3movestate = 1
@@ -218,9 +247,13 @@ def main():
                         enemy1.setFill("dark gray")
                         enemy1.setOutline("black")            
                         shaperot.undraw()
+                        shapecenter.undraw()
+                        shapecenter.setFill("#a30202")
+                        shapecenter.setOutline("#4d4d4d")
                         shaperot.setOutline("#4d4d4d")
                         shaperot.setFill("black")
                         shaperot.draw(win)
+                        shapecenter.draw(win)
 
                     if buffer == 35 and e1drawstate == 1:
                         enemy1.undraw()
@@ -245,9 +278,13 @@ def main():
                         e2movestate = 0
                     if buffer2 == 30:
                         shaperot.undraw()
+                        shapecenter.undraw()
+                        shapecenter.setFill("#a30202")
+                        shapecenter.setOutline("#4d4d4d")
                         shaperot.setOutline("#4d4d4d")
                         shaperot.setFill("black")
                         shaperot.draw(win)
+                        shapecenter.draw(win)
 
                     if buffer2 == 30 and e2drawstate == 1:
                         enemy2.undraw()
@@ -274,20 +311,14 @@ def main():
 
                 if buffer3 == 14:
                         shaperot.undraw()
+                        shapecenter.undraw()
+                        shapecenter.setFill("#a30202")
+                        shapecenter.setOutline("#4d4d4d")
                         shaperot.setOutline("#4d4d4d")
                         shaperot.setFill("black")
                         shaperot.draw(win)                 
-
-                if buffer3 == 14 or buffer == 35 or buffer2 == 30 or buffer4 == 7 and e3drawstate == 1:
-                        enemy3.undraw()
-                        enemy3.setFill("#6b0703")
-                        enemy3.setOutline("#6b0703")
-                        enemy3.draw(win)
-                elif buffer3 != 14 and buffer != 35 and buffer2 != 30 and e3drawstate == 1:
-                        enemy3.undraw()
-                        enemy3.setFill("black")
-                        enemy3.setOutline("black")
-                        enemy3.draw(win)
+                        shapecenter.draw(win)
+                
 
 
             #Enemy 4 Spawn + Move
@@ -307,22 +338,15 @@ def main():
 
                 if buffer4 == 7:
                         shaperot.undraw()
+                        shapecenter.undraw()
+                        shapecenter.setFill("#a30202")
+                        shapecenter.setOutline("#4d4d4d")
                         shaperot.setOutline("#4d4d4d")
                         shaperot.setFill("black")
-                        shaperot.draw(win)                 
+                        shaperot.draw(win)
+                        shapecenter.draw(win)
 
-                if buffer4 == 7 or buffer3 == 14 or buffer == 35 or buffer2 == 30 and e4drawstate == 1:
-                        enemy4.undraw()
-                        enemy4.setFill("#6b0703")
-                        enemy4.setOutline("#6b0703")
-                        enemy4.draw(win)
-
-                elif buffer4 != 7 and buffer3 != 14 and buffer != 35 and buffer2 != 30 and e4drawstate == 1:
-                        enemy4.undraw()
-                        enemy4.setFill("black")
-                        enemy4.setOutline("black")
-                        enemy4.draw(win)
-
+               
 
             #Power 1(5) Spawn + Move
             if enemy == 5 and p5drawstate == 0:
@@ -340,10 +364,14 @@ def main():
 
                 if buffer5 == 40:
                         shaperot.undraw()
+                        shapecenter.undraw()
+                        shapecenter.setFill("green")
+                        shapecenter.setOutline("silver")
                         shaperot.setOutline("silver")
                         shaperot.setFill("purple")
                         shaperot.setWidth(5)
                         shaperot.draw(win)
+                        shapecenter.draw(win)
                         armorstate = (armorstate) + 1
                         arm = int(arm) + 1
                         armor.undraw()
@@ -389,6 +417,7 @@ def main():
                 shapeleft.undraw()
                 shaperight.undraw()
                 shapeup.undraw()
+                shapecenter.undraw()
                 enemy1.move(100,0)
                 tfour.draw(win)
                 win.getMouse()
@@ -427,6 +456,7 @@ def main():
                 shapeleft.undraw()
                 shaperight.undraw()
                 shapeup.undraw()
+                shapecenter.undraw()
                 enemy2.move(-100,0)
                 tfour.draw(win)                    
                 win.getMouse()
@@ -464,6 +494,7 @@ def main():
                 shapeleft.undraw()
                 shaperight.undraw()
                 shapeup.undraw()
+                shapecenter.undraw()
                 enemy3.move(0,-100)
                 tfour.draw(win)
                 win.getMouse()
@@ -501,6 +532,7 @@ def main():
                 shapeleft.undraw()
                 shaperight.undraw()
                 shapeup.undraw()
+                shapecenter.undraw()
                 enemy4.move(0,100)
                 tfour.draw(win)
                 win.getMouse()
@@ -518,12 +550,47 @@ def main():
                 armor.setText("ARMOR: " + str(arm))
                 armor.draw(win)
            
-                
+
+
+
+
+           
+
+
+            
+
+
+
             if buffer == 0 or buffer2 == 0 or buffer3 == 0 or buffer4 == 0:
                 win.setBackground("black")
+                if e3drawstate == 1:
+                    enemy3.undraw()
+                    enemy3.setOutline("black")
+                    enemy3.setFill("black")
+                    enemy3.draw(win)
+                if e4drawstate == 1:
+                    enemy4.undraw()
+                    enemy4.setFill("black")
+                    enemy4.setOutline("black")
+                    enemy4.draw(win)
             if buffer == 35 or buffer2 == 30 or buffer3 == 14 or buffer4 == 7:
                 win.setBackground("#a30202")
-            
+                if e3drawstate == 1:
+                    enemy3.undraw()
+                    enemy3.setFill("#6d0000")
+                    enemy3.setOutline("#6d0000")
+                    enemy3.draw(win)
+                if e4drawstate == 1:
+                    enemy4.undraw()
+                    enemy4.setFill("#6d0000")
+                    enemy4.setOutline("#6d0000")
+                    enemy4.draw(win)
+            if pts == 75:
+                e1drawstate = 3
+                e2drawstate = 3
+                e3drawstate = 3
+                e4drawstate = 3
+                p5drawstate = 3
                 
 
         
